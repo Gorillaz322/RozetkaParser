b37cf271e40a374df061b88e8edc574093f79ea2
Vadim <jyvylo5@gmail.com>
2016-06-27 23:45:40 +0300

    Changed crontab

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ celery = make_celery(app)
 CELERYBEAT_SCHEDULE = {
     'save_products': {
         'task': 'tasks.update_products_main_task',
-        'schedule': crontab(minute=50, hour=21),
+        'schedule': crontab(minute=50, hour=20),
     }
 }
 
