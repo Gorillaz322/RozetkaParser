00f613163ffb619b6cfac7bf0f4aedb337aef51b
Vadim <jyvylo5@gmail.com>
2016-06-27 17:05:51 +0300

    Changed scheduler

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ celery = make_celery(app)
 CELERYBEAT_SCHEDULE = {
     'save_products': {
         'task': 'tasks.update_products_main_task',
-        'schedule': timedelta(minutes=30),
+        'schedule': timedelta(minutes=1),
     }
 }
 
